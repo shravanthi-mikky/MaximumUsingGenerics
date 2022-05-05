1bd9722fe64e28719b0e53fc310a1500cba8ceb2
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-05 15:09:48 +0530

    [shravanthi] - using float values

--- MaxMethodUsingGenerics/Program.cs
+++ MaxMethodUsingGenerics/Program.cs
@@ -2,6 +2,25 @@
 using MaxMethodUsingGenerics;
 
 Console.WriteLine("Welcome to MAximum Method Using Generics!");
-UsingInt obj1 = new UsingInt();
-int maxValueInt = obj1.MaxValueInt(40, 20, 10);
-Console.WriteLine("Maximum value when comparing three integers is : " + maxValueInt);
+while(true)
+{
+    Console.WriteLine("PLease choose the option :");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            UsingInt obj1 = new UsingInt();
+            int maxValueInt = obj1.MaxValueInt(40, 20, 10);
+            Console.WriteLine("Maximum value when comparing three integers is : " + maxValueInt);
+            break;
+        case 2:
+            UsingFloat obj2 = new UsingFloat();
+            double maxValueFloat = obj2.MaxValueFloat(1.2, 2.4, 8.9);
+            Console.WriteLine("Maximum value when comparing three integers is : " + maxValueFloat);
+            break;
+        default:
+            break;
+
+    }
+}
+;
