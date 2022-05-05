b9cbab7722b88fdf979747e1a3d41278ffb9e576
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-05 14:56:45 +0530

    [shravanthi] Maximum using integers

---
+++ MaxMethodUsingGenerics/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MaxMethodUsingGenerics;
+
+Console.WriteLine("Welcome to MAximum Method Using Generics!");
+UsingInt obj1 = new UsingInt();
+int maxValueInt = obj1.MaxValueInt(40, 20, 10);
+Console.WriteLine("Maximum value when comparing three integers is : " + maxValueInt);
